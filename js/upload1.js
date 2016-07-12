af3b4ea4b20bc50727a95242f6b70197da786dce
wantings <jianyujingjing@sina.cn>
2016-07-12 16:00:16 +0800

    the first 文件上传功能

---
+++ js/upload1.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jianyujing on 2016/6/30.
+ */
+$.ajax({
+
+
+
+
+
+})
---
+++ js/upload2.js
@@ -0,0 +1,6 @@
+/**
+ * Created by jianyujing on 2016/6/30.
+ */
+var formData=new formData();
+formData.append('file',$);
+$.ajax()
